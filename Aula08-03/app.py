94dce9d6dd4f64518c79a7eb7e87211ddb23ccde
Y4nnLS <yann_lucas04@hotmail.com>
2024-03-15 14:39:18 -0300

    a

--- Aula08-03/app.py
+++ Aula08-03/app.py
@@ -52,7 +52,9 @@ dados_numericos_legiveis = pd.DataFrame(modelo_normalizador.inverse_transform(da
 
 # Juntar com os dados categóricos normalizados
 dados_legiveis_final = dados_numericos_legiveis.join(dados_categoricos_normalizados, how='left')
-
+categorias_originais = dados_categoricos_normalizados.idxmax(axis=1).str.replace('sexo_', '')
+dados_legiveis_final['sexo'] = categorias_originais
+dados_legiveis_final = dados_legiveis_final.drop(columns=['sexo_F','sexo_M'])
 print(dados_legiveis_final.head(10))
 
 
